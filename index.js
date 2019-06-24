8ea182768ab1c2d98d6e4afa8904f613e0214d7f
Paul Yun <p.yun1994@gmail.com>
2019-06-24 18:11:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ function removeFromCart(item) {
       return cart;
     }
   }
-  return 'This item is not in your cart.'
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
