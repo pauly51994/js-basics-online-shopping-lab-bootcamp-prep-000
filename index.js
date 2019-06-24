d363a4a05b69f156cb1f2e74fc54867d5679e257
Paul Yun <p.yun1994@gmail.com>
2019-06-24 18:04:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ function removeFromCart(item) {
   for(let i = 0; i < cart.length; i++){
     if (cart[i].itemName === item){
       cart.splice(i,1);
-      return cart;
+      console.log(cart);
     } else {return 'That item is not in your cart.'}
   }
 }
